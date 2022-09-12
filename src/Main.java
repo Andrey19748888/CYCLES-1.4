8d7fd3321b951b211731b58f52836f2668c5b0f4
Andreydenisov7788 <asasa88881974@mail.ru>
2022-09-12 21:21:44 +0300

    дз 2, задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        for (int i = 0; i < 10; i ++) {
-            System.out.println(" первая задача" + i);
+        for (int i = 1; i < 513; i = i * 2) {
+            System.out.println(i);
         }
     }
 }
